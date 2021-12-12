e4c907738e3a471818f063990ab4ee3b936d07c9
Andy Heller <andy.heller@arcticwolf.com>
2021-12-12 00:44:59 -0500

    fmt 11 and 12
    **Description:**
    
    **Testing:**

--- day11/main.go
+++ day11/main.go
@@ -8,12 +8,12 @@ import (
 )
 
 const (
-  steps = 100
+	steps = 100
 )
 
 type Point struct {
-  Val int
-  Flashed int
+	Val     int
+	Flashed int
 }
 
 type Grid struct {
@@ -21,63 +21,63 @@ type Grid struct {
 }
 
 func (g *Grid) Print() {
-  for y := range g.Points {
-    for x := range g.Points[y] {
-      fmt.Printf("%v", g.Points[y][x].Flashed)
-    }
-    fmt.Println()
-  }
-  for y := range g.Points {
-    for x := range g.Points[y] {
-      if g.Points[y][x].Val > 9 {
-        fmt.Printf("F")
-      } else {
-        fmt.Printf("%v", g.Points[y][x].Val)
-      }
-    }
-    fmt.Println()
-  }
+	for y := range g.Points {
+		for x := range g.Points[y] {
+			fmt.Printf("%v", g.Points[y][x].Flashed)
+		}
+		fmt.Println()
+	}
+	for y := range g.Points {
+		for x := range g.Points[y] {
+			if g.Points[y][x].Val > 9 {
+				fmt.Printf("F")
+			} else {
+				fmt.Printf("%v", g.Points[y][x].Val)
+			}
+		}
+		fmt.Println()
+	}
 }
 
 func (g *Grid) FlashPoint(x, y, round int) {
-  for i := -1; i < 2; i++ {
-    for t := -1; t < 2; t++ {
-      if y+i < 0 || y+i >= len(g.Points) || x+t < 0 || x+t >= len(g.Points[y+i]) {
-        continue
-      }
-      g.Points[y+i][x+t].Val += 1
-      if g.Points[y+i][x+t].Val > 9 && g.Points[y+i][x+t].Flashed < round {
-        g.Points[y+i][x+t].Flashed = round
-        g.FlashPoint(x+t, y+i, round)
-      }
-    }
-  }
+	for i := -1; i < 2; i++ {
+		for t := -1; t < 2; t++ {
+			if y+i < 0 || y+i >= len(g.Points) || x+t < 0 || x+t >= len(g.Points[y+i]) {
+				continue
+			}
+			g.Points[y+i][x+t].Val += 1
+			if g.Points[y+i][x+t].Val > 9 && g.Points[y+i][x+t].Flashed < round {
+				g.Points[y+i][x+t].Flashed = round
+				g.FlashPoint(x+t, y+i, round)
+			}
+		}
+	}
 }
 
 func (g *Grid) Flash(round int) int {
-  for y := range g.Points {
-    for x := range g.Points[y] {
-      g.Points[y][x].Val += 1
-    }
-  }
-  for y := range g.Points {
-    for x := range g.Points[y] {
-      if g.Points[y][x].Val > 9 && g.Points[y][x].Flashed < round {
-        g.Points[y][x].Flashed = round
-        g.FlashPoint(x, y, round)
-      }
-    }
-  }
-  count := 0
-  for y := range g.Points {
-    for x := range g.Points[y] {
-      if g.Points[y][x].Val > 9 && g.Points[y][x].Flashed == round {
-        g.Points[y][x].Val = 0
-        count += 1
-      }
-    }
-  }
-  return count
+	for y := range g.Points {
+		for x := range g.Points[y] {
+			g.Points[y][x].Val += 1
+		}
+	}
+	for y := range g.Points {
+		for x := range g.Points[y] {
+			if g.Points[y][x].Val > 9 && g.Points[y][x].Flashed < round {
+				g.Points[y][x].Flashed = round
+				g.FlashPoint(x, y, round)
+			}
+		}
+	}
+	count := 0
+	for y := range g.Points {
+		for x := range g.Points[y] {
+			if g.Points[y][x].Val > 9 && g.Points[y][x].Flashed == round {
+				g.Points[y][x].Val = 0
+				count += 1
+			}
+		}
+	}
+	return count
 }
 
 func main() {
@@ -87,7 +87,7 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-  var g Grid
+	var g Grid
 	for y, line := range bytes.Split(contents, []byte("\n")) {
 		if len(line) == 0 {
 			continue
@@ -95,19 +95,19 @@ func main() {
 		g.Points = append(g.Points, []*Point{})
 		for _, r := range string(line) {
 			val, _ := strconv.Atoi(string(r))
-      g.Points[y] = append(g.Points[y], &Point{Val: val})
-    }
-  }
-  sum := 0
-  for i := 0;; i++ {
-    flashing := g.Flash(i+1)
-    if i < steps {
-      sum += flashing
-    }
-    if flashing == len(g.Points)*len(g.Points[0]) {
-      fmt.Println(sum)
-      fmt.Println(i+1)
-      break
-    }
-  }
+			g.Points[y] = append(g.Points[y], &Point{Val: val})
+		}
+	}
+	sum := 0
+	for i := 0; ; i++ {
+		flashing := g.Flash(i + 1)
+		if i < steps {
+			sum += flashing
+		}
+		if flashing == len(g.Points)*len(g.Points[0]) {
+			fmt.Println(sum)
+			fmt.Println(i + 1)
+			break
+		}
+	}
 }
--- day12/main.go
+++ day12/main.go
@@ -4,13 +4,13 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
-  "strings"
+	"strings"
 )
 
 type Node struct {
-  Big bool
-  Name string
-  Links []string
+	Big   bool
+	Name  string
+	Links []string
 }
 
 //type Link struct {
@@ -18,31 +18,31 @@ type Node struct {
 //  End *Node
 //}
 
-func paths (start *Node, visited *map[string]bool, nodes *map[string]*Node, double bool) int {
-  if start.Name == "end" {
-    return 1
-  }
-  localDub := double
-  if _, ok := (*visited)[start.Name]; ok && !start.Big {
-    if double == false && start.Name != "start"{
-      localDub = true
-    } else {
-      return 0
-    }
-  }
-  visits := 0
-  local := make(map[string]bool)
-  for c := range (*visited) {
-    local[c] = true
-  }
+func paths(start *Node, visited *map[string]bool, nodes *map[string]*Node, double bool) int {
+	if start.Name == "end" {
+		return 1
+	}
+	localDub := double
+	if _, ok := (*visited)[start.Name]; ok && !start.Big {
+		if double == false && start.Name != "start" {
+			localDub = true
+		} else {
+			return 0
+		}
+	}
+	visits := 0
+	local := make(map[string]bool)
+	for c := range *visited {
+		local[c] = true
+	}
 
-  if !start.Big {
-    local[start.Name] = true
-  }
-  for _, child := range start.Links {
-    visits += paths((*nodes)[child], &local, nodes, localDub)
-  }
-  return visits
+	if !start.Big {
+		local[start.Name] = true
+	}
+	for _, child := range start.Links {
+		visits += paths((*nodes)[child], &local, nodes, localDub)
+	}
+	return visits
 }
 
 func main() {
@@ -54,28 +54,28 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-  nodes := make(map[string]*Node)
+	nodes := make(map[string]*Node)
 	for _, line := range bytes.Split(contents, []byte("\n")) {
 		if len(line) == 0 {
 			continue
 		}
-	  path := bytes.Split(line, []byte("-"))
-    a := string(path[0])
-    b := string(path[1])
-    if _, ok := nodes[a]; !ok {
-      nodes[a] = &Node{Name: a, Links: []string{b}, Big: strings.ToUpper(a) == a }
-    } else {
-      nodes[a].Links = append(nodes[a].Links, b)
-    }
-    if _, ok := nodes[b]; !ok {
-      nodes[b] = &Node{Name: b, Links: []string{a}, Big: strings.ToUpper(b) == b}
-    } else {
-      nodes[b].Links = append(nodes[b].Links, a)
-    }
-  }
-  var start = nodes["start"]
-  visited := make(map[string]bool)
-  fmt.Println(paths(start, &visited, &nodes, true))
-  visited = make(map[string]bool)
-  fmt.Println(paths(start, &visited, &nodes, false))
+		path := bytes.Split(line, []byte("-"))
+		a := string(path[0])
+		b := string(path[1])
+		if _, ok := nodes[a]; !ok {
+			nodes[a] = &Node{Name: a, Links: []string{b}, Big: strings.ToUpper(a) == a}
+		} else {
+			nodes[a].Links = append(nodes[a].Links, b)
+		}
+		if _, ok := nodes[b]; !ok {
+			nodes[b] = &Node{Name: b, Links: []string{a}, Big: strings.ToUpper(b) == b}
+		} else {
+			nodes[b].Links = append(nodes[b].Links, a)
+		}
+	}
+	var start = nodes["start"]
+	visited := make(map[string]bool)
+	fmt.Println(paths(start, &visited, &nodes, true))
+	visited = make(map[string]bool)
+	fmt.Println(paths(start, &visited, &nodes, false))
 }
