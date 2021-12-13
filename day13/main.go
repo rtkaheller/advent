a2446f60e39fa5e9f532e2975212f6287a88efa1
Andy Heller <andy.heller@arcticwolf.com>
2021-12-13 00:43:46 -0500

    fmt 13

--- day13/main.go
+++ day13/main.go
@@ -4,30 +4,30 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
-  "strconv"
+	"strconv"
 )
 
 type Coord struct {
-  X, Y int
+	X, Y int
 }
 
 type Fold struct {
-  Up bool
-  Axis int
+	Up   bool
+	Axis int
 }
 
-func fold (dots []*Coord, f *Fold) {
-  for _, dot := range dots {
-    if f.Up {
-      if dot.Y > f.Axis {
-        dot.Y = f.Axis - (dot.Y - f.Axis)
-      }
-    } else {
-      if dot.X > f.Axis {
-        dot.X = f.Axis - (dot.X - f.Axis)
-      }
-    }
-  }
+func fold(dots []*Coord, f *Fold) {
+	for _, dot := range dots {
+		if f.Up {
+			if dot.Y > f.Axis {
+				dot.Y = f.Axis - (dot.Y - f.Axis)
+			}
+		} else {
+			if dot.X > f.Axis {
+				dot.X = f.Axis - (dot.X - f.Axis)
+			}
+		}
+	}
 }
 
 func main() {
@@ -37,57 +37,57 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-  var dots []*Coord
-  var start int
+	var dots []*Coord
+	var start int
 	for resume, line := range bytes.Split(contents, []byte("\n")) {
 		if len(line) == 0 {
-      start = resume
+			start = resume
 			break
 		}
-    coords := bytes.Split(line, []byte(","))
+		coords := bytes.Split(line, []byte(","))
 
-    coord := new(Coord)
+		coord := new(Coord)
 
-    coord.X, _ = strconv.Atoi(string(coords[0]))
-    coord.Y, _ = strconv.Atoi(string(coords[1]))
-    dots = append(dots, coord)
-  }
-  var folds []*Fold
+		coord.X, _ = strconv.Atoi(string(coords[0]))
+		coord.Y, _ = strconv.Atoi(string(coords[1]))
+		dots = append(dots, coord)
+	}
+	var folds []*Fold
 	for i, line := range bytes.Split(contents, []byte("\n")) {
-    if i <= start {
-      continue
-    } else if len(line) == 0 {
-      continue
-    }
-    words := bytes.Split(line, []byte(" "))
-    f := bytes.Split(words[2], []byte("="))
-    fold := new(Fold)
-    fold.Up = (string(f[0]) == "y")
-    fold.Axis, _ = strconv.Atoi(string(f[1]))
-    folds = append(folds, fold)
-  }
-  for _, f := range folds {
-    fold(dots, f)
-  }
-  paper := make(map[string]bool)
-  var maxX, maxY int
-  for _, dot := range dots {
-    if dot.X > maxX {
-      maxX = dot.X
-    }
-    if dot.Y > maxY {
-      maxY = dot.Y
-    }
-    paper[fmt.Sprintf("%v,%v", dot.X, dot.Y)] = true
-  }
-  for y := 0; y < maxY+1; y++ {
-    for x := 0; x < maxX; x++ {
-      if paper[fmt.Sprintf("%v,%v", x, y)] {
-        fmt.Printf("#")
-      } else {
-        fmt.Printf(".")
-      }
-    }
-    fmt.Println()
-  }
+		if i <= start {
+			continue
+		} else if len(line) == 0 {
+			continue
+		}
+		words := bytes.Split(line, []byte(" "))
+		f := bytes.Split(words[2], []byte("="))
+		fold := new(Fold)
+		fold.Up = (string(f[0]) == "y")
+		fold.Axis, _ = strconv.Atoi(string(f[1]))
+		folds = append(folds, fold)
+	}
+	for _, f := range folds {
+		fold(dots, f)
+	}
+	paper := make(map[string]bool)
+	var maxX, maxY int
+	for _, dot := range dots {
+		if dot.X > maxX {
+			maxX = dot.X
+		}
+		if dot.Y > maxY {
+			maxY = dot.Y
+		}
+		paper[fmt.Sprintf("%v,%v", dot.X, dot.Y)] = true
+	}
+	for y := 0; y < maxY+1; y++ {
+		for x := 0; x < maxX; x++ {
+			if paper[fmt.Sprintf("%v,%v", x, y)] {
+				fmt.Printf("#")
+			} else {
+				fmt.Printf(".")
+			}
+		}
+		fmt.Println()
+	}
 }
