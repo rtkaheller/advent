8afb1ed73d21c9064828d686d59c78861f06f35d
Andy Heller <andy.heller@arcticwolf.com>
2022-12-11 02:03:15 -0500

    2022 day11 - python3
    **Description:**
    
    **Testing:**

--- 2022/day11/main.py
+++ 2022/day11/main.py
@@ -1,15 +1,15 @@
 def inspect(monkeys, m, relief, gcd):
   for old in m["items"]:
     m["inspects"] += 1
-    exec(m["op"])
-    new /= relief
+    new = int(eval(m["op"].split('=')[1]))
+    new //= relief
     if new % m["test"] == 0:
       monkeys[m["true"]]["items"].append(new % gcd)
     else:
       monkeys[m["false"]]["items"].append(new % gcd)
   m["items"] = []
 
-    
+
 def main(file, rounds, relief):
   monkeys = []
   with open(file, "r") as f:
@@ -26,7 +26,10 @@ def main(file, rounds, relief):
         monkey["false"] = int(lines[i+5][29:].strip())
         monkeys.append(monkey)
 
-  gcd = reduce(lambda x, y: x*y, (m["test"] for m in monkeys))
+  gcd = 1
+  for m in monkeys:
+    gcd *= m["test"]
+
   for i in range(rounds):
     counts = []
     for m in monkeys:
@@ -35,10 +38,9 @@ def main(file, rounds, relief):
   counts.sort()
 
   return counts[-1] * counts[-2]
-  
+
 
 if __name__ == "__main__":
   file = "input.txt"
   print(main(file, 20, 3))
   print(main(file, 10000, 1))
-
